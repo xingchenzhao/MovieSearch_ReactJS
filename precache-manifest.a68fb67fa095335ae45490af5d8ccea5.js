93299398f61d68dc16911015484688b2a6e775fa
xingchenzhao <xiz168@pitt.edu>
2019-05-19 00:54:36 -0400

    Updates

---
+++ precache-manifest.a68fb67fa095335ae45490af5d8ccea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac7ed45dc0bec06f346b",
+    "url": "/MovieSearch_ReactJS/static/js/runtime~main.27d84e93.js"
+  },
+  {
+    "revision": "d54a440ae4a886cf6aa3",
+    "url": "/MovieSearch_ReactJS/static/js/main.1b83e055.chunk.js"
+  },
+  {
+    "revision": "59043e73e9cf2e466698",
+    "url": "/MovieSearch_ReactJS/static/js/2.868f3343.chunk.js"
+  },
+  {
+    "revision": "d54a440ae4a886cf6aa3",
+    "url": "/MovieSearch_ReactJS/static/css/main.6c722af4.chunk.css"
+  },
+  {
+    "revision": "a5b6d5b9abb9e48763c1bdb1a011b77a",
+    "url": "/MovieSearch_ReactJS/index.html"
+  }
+];
